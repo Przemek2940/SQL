3e2c470eac8b4c07065d3bae1bc15a596e8b0879
Przemek2940 <89359034+Przemek2940@users.noreply.github.com>
2022-08-15 13:14:56 +0200

    Update Wydruk.sql
    Zakrycie danych

--- Wydruk.sql
+++ Wydruk.sql
@@ -1,88 +1,88 @@
 select distinct k.opis, ir3.nazwa_rodzaju,
 (select sum(dp.cena-dp.korekta_ceny) as eksport
-	from g.gm_dokumenty_naglowki dn
-		left join g.gm_dokumenty_pozycje dp on dp.id_naglowka = dn.id
-		left join g.spd_kontrahenci kon on dn.kontrahent = kon.id_kontrahenta
-		left join g.gm_indeksy ind on dp.id_indeksu = ind.id_indeksu
-		left join g.ks_katalogi_relacje kkr on ind.id_indeksu = kkr.id_towaru
-		left join g.ks_katalogi kk on kkr.id_naglowka = kk.id
+	from TABELA dn
+		left join TABELA dp on dp.id_naglowka = dn.id
+		left join TABELA kon on dn.kontrahent = kon.id_kontrahenta
+		left join TABELA ind on dp.id_indeksu = ind.id_indeksu
+		left join TABELA kkr on ind.id_indeksu = kkr.id_towaru
+		left join TABELA kk on kkr.id_naglowka = kk.id
 	where dn.symbol = 'Wz' and dn.rok = dnn.rok and dn.miesiac = dnn.miesiac
 		and kon.id_rodzaju2 = 4 and kon.id_rodzaju1 = kont.id_rodzaju1
 		and kkr.id_naglowka = kr.id_naglowka and ind.rodzaj_materialu3 = i.rodzaj_materialu3),
 
 (select sum(dp.cena-dp.korekta_ceny) as pozkontr
-	from g.gm_dokumenty_naglowki dn
-		left join g.gm_dokumenty_pozycje dp on dp.id_naglowka = dn.id
-		left join g.spd_kontrahenci kon on dn.kontrahent = kon.id_kontrahenta
-		left join g.gm_indeksy ind on dp.id_indeksu = ind.id_indeksu
-		left join g.ks_katalogi_relacje kkr on ind.id_indeksu = kkr.id_towaru
-		left join g.ks_katalogi kk on kkr.id_naglowka = kk.id
+	from TABELA dn
+		left join TABELA dp on dp.id_naglowka = dn.id
+		left join TABELA kon on dn.kontrahent = kon.id_kontrahenta
+		left join TABELA ind on dp.id_indeksu = ind.id_indeksu
+		left join TABELA kkr on ind.id_indeksu = kkr.id_towaru
+		left join TABELA kk on kkr.id_naglowka = kk.id
 	where dn.symbol = 'Wz' and dn.rok = dnn.rok and dn.miesiac = dnn.miesiac
 		and kon.id_rodzaju2 = 7 and kon.id_rodzaju1 = kont.id_rodzaju1
 		and kkr.id_naglowka = kr.id_naglowka and ind.rodzaj_materialu3 = i.rodzaj_materialu3),
 
 (select sum(dp.cena-dp.korekta_ceny) as region1
-	from g.gm_dokumenty_naglowki dn
-		left join g.gm_dokumenty_pozycje dp on dp.id_naglowka = dn.id
-		left join g.spd_kontrahenci kon on dn.kontrahent = kon.id_kontrahenta
-		left join g.gm_indeksy ind on dp.id_indeksu = ind.id_indeksu
-		left join g.ks_katalogi_relacje kkr on ind.id_indeksu = kkr.id_towaru
-		left join g.ks_katalogi kk on kkr.id_naglowka = kk.id
+	from TABELA dn
+		left join TABELA dp on dp.id_naglowka = dn.id
+		left join TABELA kon on dn.kontrahent = kon.id_kontrahenta
+		left join TABELA ind on dp.id_indeksu = ind.id_indeksu
+		left join TABELA kkr on ind.id_indeksu = kkr.id_towaru
+		left join TABELA kk on kkr.id_naglowka = kk.id
 	where dn.symbol = 'Wz' and dn.rok = dnn.rok and dn.miesiac = dnn.miesiac
 		and kon.id_rodzaju2 = 1 and kon.id_rodzaju1 = kont.id_rodzaju1
 		and kkr.id_naglowka = kr.id_naglowka and ind.rodzaj_materialu3 = i.rodzaj_materialu3),
 
 (select sum(dp.cena-dp.korekta_ceny) as region2
-	from g.gm_dokumenty_naglowki dn
-		left join g.gm_dokumenty_pozycje dp on dp.id_naglowka = dn.id
-		left join g.spd_kontrahenci kon on dn.kontrahent = kon.id_kontrahenta
-		left join g.gm_indeksy ind on dp.id_indeksu = ind.id_indeksu
-		left join g.ks_katalogi_relacje kkr on ind.id_indeksu = kkr.id_towaru
-		left join g.ks_katalogi kk on kkr.id_naglowka = kk.id
+	from TABELA dn
+		left join TABELA dp on dp.id_naglowka = dn.id
+		left join TABELA kon on dn.kontrahent = kon.id_kontrahenta
+		left join TABELA ind on dp.id_indeksu = ind.id_indeksu
+		left join TABELA kkr on ind.id_indeksu = kkr.id_towaru
+		left join TABELA kk on kkr.id_naglowka = kk.id
 	where dn.symbol = 'Wz' and dn.rok = dnn.rok and dn.miesiac = dnn.miesiac
 		and kon.id_rodzaju2 = 2 and kon.id_rodzaju1 = kont.id_rodzaju1
 		and kkr.id_naglowka = kr.id_naglowka and ind.rodzaj_materialu3 = i.rodzaj_materialu3),
 
 (select sum(dp.cena-dp.korekta_ceny) as region3
-	from g.gm_dokumenty_naglowki dn
-		left join g.gm_dokumenty_pozycje dp on dp.id_naglowka = dn.id
-		left join g.spd_kontrahenci kon on dn.kontrahent = kon.id_kontrahenta
-		left join g.gm_indeksy ind on dp.id_indeksu = ind.id_indeksu
-		left join g.ks_katalogi_relacje kkr on ind.id_indeksu = kkr.id_towaru
-		left join g.ks_katalogi kk on kkr.id_naglowka = kk.id
+	from TABELA dn
+		left join TABELA dp on dp.id_naglowka = dn.id
+		left join TABELA kon on dn.kontrahent = kon.id_kontrahenta
+		left join TABELAind on dp.id_indeksu = ind.id_indeksu
+		left join TABELA kkr on ind.id_indeksu = kkr.id_towaru
+		left join TABELA kk on kkr.id_naglowka = kk.id
 	where dn.symbol = 'Wz' and dn.rok = dnn.rok and dn.miesiac = dnn.miesiac
 		and kon.id_rodzaju2 = 3 and kon.id_rodzaju1 = kont.id_rodzaju1
 		and kkr.id_naglowka = kr.id_naglowka and ind.rodzaj_materialu3 = i.rodzaj_materialu3),
 
 (select sum(dp.cena-dp.korekta_ceny) as region4
-	from g.gm_dokumenty_naglowki dn
-		left join g.gm_dokumenty_pozycje dp on dp.id_naglowka = dn.id
-		left join g.spd_kontrahenci kon on dn.kontrahent = kon.id_kontrahenta
-		left join g.gm_indeksy ind on dp.id_indeksu = ind.id_indeksu
-		left join g.ks_katalogi_relacje kkr on ind.id_indeksu = kkr.id_towaru
-		left join g.ks_katalogi kk on kkr.id_naglowka = kk.id
+	from TABELA dn
+		left join TABELA dp on dp.id_naglowka = dn.id
+		left join TABELA kon on dn.kontrahent = kon.id_kontrahenta
+		left join TABELA ind on dp.id_indeksu = ind.id_indeksu
+		left join TABELA kkr on ind.id_indeksu = kkr.id_towaru
+		left join TABELA kk on kkr.id_naglowka = kk.id
 	where dn.symbol = 'Wz' and dn.rok = dnn.rok and dn.miesiac = dnn.miesiac
 		and kon.id_rodzaju2 = 10 and kon.id_rodzaju1 = kont.id_rodzaju1
 		and kkr.id_naglowka = kr.id_naglowka and ind.rodzaj_materialu3 = i.rodzaj_materialu3),
 
 (select sum(dp.cena-dp.korekta_ceny) as region5
-	from g.gm_dokumenty_naglowki dn
-		left join g.gm_dokumenty_pozycje dp on dp.id_naglowka = dn.id
-		left join g.spd_kontrahenci kon on dn.kontrahent = kon.id_kontrahenta
-		left join g.gm_indeksy ind on dp.id_indeksu = ind.id_indeksu
-		left join g.ks_katalogi_relacje kkr on ind.id_indeksu = kkr.id_towaru
-		left join g.ks_katalogi kk on kkr.id_naglowka = kk.id
+	from TABELA dn
+		left join TABELA dp on dp.id_naglowka = dn.id
+		left join TABELA kon on dn.kontrahent = kon.id_kontrahenta
+		left join TABELA ind on dp.id_indeksu = ind.id_indeksu
+		left join TABELA kkr on ind.id_indeksu = kkr.id_towaru
+		left join TABELA kk on kkr.id_naglowka = kk.id
 	where dn.symbol = 'Wz' and dn.rok = dnn.rok and dn.miesiac = dnn.miesiac
 		and kon.id_rodzaju2 = 11 and kon.id_rodzaju1 = kont.id_rodzaju1
 		and kkr.id_naglowka = kr.id_naglowka and ind.rodzaj_materialu3 = i.rodzaj_materialu3)
 
-from g.gm_indeksy i
-	left join g.ks_katalogi_relacje kr on i.id_indeksu = kr.id_towaru
-	left join g.ks_katalogi k on kr.id_naglowka = k.id
-	left join g.gm_dokumenty_pozycje dpp on i.id_indeksu = dpp.id_indeksu
-	left join g.gm_dokumenty_naglowki dnn on dpp.id_naglowka = dnn.id
-	left join g.spd_kontrahenci kont on dnn.kontrahent = kont.id_kontrahenta
-	left join g.gm_indeksy_rodzaj3  ir3 on ir3.id_rodzaju = i.rodzaj_materialu3
+from TABELA i
+	left join TABELA kr on i.id_indeksu = kr.id_towaru
+	left join TABELA k on kr.id_naglowka = k.id
+	left join TABELA dpp on i.id_indeksu = dpp.id_indeksu
+	left join TABELA dnn on dpp.id_naglowka = dnn.id
+	left join TABELA kont on dnn.kontrahent = kont.id_kontrahenta
+	left join TABELA  ir3 on ir3.id_rodzaju = i.rodzaj_materialu3
 where dnn.symbol = 'Wz'
 	and dnn.rok = @PARAMETR1@
 	and dnn.miesiac =  @PARAMETR2@
